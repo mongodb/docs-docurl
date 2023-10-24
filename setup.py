0d809408d4758f457949683fd1071b9faf1ea8c3
Nathan Leniz <nathan.leniz@gmail.com>
2023-10-24 12:37:13 -0700

    next bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="docurl",
-    version="0.5.2",
+    version="0.5.3",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/mongodb/docs-docurl",
