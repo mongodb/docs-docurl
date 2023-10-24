c1b5911df89af3b170f50394310bc2af5a47009f
Nathan Leniz <nathan.leniz@gmail.com>
2023-10-24 12:32:52 -0700

    update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="docurl",
-    version="0.4.0",
+    version="0.5.2",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/mongodb/docs-docurl",
